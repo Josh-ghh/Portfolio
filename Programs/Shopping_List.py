c5726c0ad1009225a0d74010881bd552819a6bd2
Josh-ghh <joshua.bradford@outlook.com>
2025-01-28 19:41:34 +0000

    Update Shopping_List.py

--- Programs/Shopping_List.py
+++ Programs/Shopping_List.py
@@ -17,17 +17,28 @@ def show_list(s_list):
     else:
         print(s_list)
 
+def remove_item(s_list):
+try:
+        amount = int(input("How many items do you want to remove from the list?  "))
+        for x in range(amount):
+            remove = input(" enter an item to remove ")
+            s_list.pop(remove)
+            print(s_list)
+    except ValueError:
+        print("invalid response, exiting list removal")
 
 def menu():
     z = True
     while z == True:
         try:
-            pick = int(input("Pick a number for what you'd like to do. 1. Show the list, 2. add to the list, 3. exit"))
+            pick = int(input("Pick a number for what you'd like to do. 1. Show the list, 2. add to the list, 3. remove an item from the list, 4. exit"))
             if pick == 1:
                 show_list(s_list)
             elif pick == 2:
                 shop_add(s_list)
             elif pick == 3:
+                    remove_item(s_list)
+            elif pick == 4:
                 z = False
                 print("exiting")
             else:
