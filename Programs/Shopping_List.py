409d980a795f3dc6853ebfd75361831cb469d1a5
Reece Atkinson <hello@reeceatkinson.com>
2025-01-29 10:00:42 +0000

    Update Shopping_List.py

--- Programs/Shopping_List.py
+++ Programs/Shopping_List.py
@@ -1,29 +1,29 @@
-s_list=[ ]
-def shop_add(s_list):
+shopping_list=[ ]
+def shop_add(shopping_list):
         try:
             amount = int(input("How many items do you want to add to the list?  "))
 
             for x in range(amount):
                 add = input("Add to the shopping list: ")
-                s_list.append(add)
-                print(s_list)
+                shopping_list.append(add)
+                print(shopping_list)
         except ValueError:
             print("invalid response, exiting list adding")
 
 
-def show_list(s_list):
-    if len(s_list) == 0:
+def show_list(shopping_list):
+    if len(shopping_list) == 0:
         print("The shopping list is empty.")
     else:
-        print(s_list)
+        print(shopping_list)
 
-def remove_item(s_list):
+def remove_item(shopping_list):
 try:
         amount = int(input("How many items do you want to remove from the list?  "))
         for x in range(amount):
             remove = input(" enter an item to remove ")
-            s_list.pop(remove)
-            print(s_list)
+            shopping_list.pop(remove)
+            print(shopping_list)
     except ValueError:
         print("invalid response, exiting list removal")
 
@@ -33,11 +33,11 @@ def menu():
         try:
             pick = int(input("Pick a number for what you'd like to do. 1. Show the list, 2. add to the list, 3. remove an item from the list, 4. exit"))
             if pick == 1:
-                show_list(s_list)
+                show_list(shopping_list)
             elif pick == 2:
-                shop_add(s_list)
+                shop_add(shopping_list)
             elif pick == 3:
-                    remove_item(s_list)
+                    remove_item(shopping_list)
             elif pick == 4:
                 z = False
                 print("exiting")
